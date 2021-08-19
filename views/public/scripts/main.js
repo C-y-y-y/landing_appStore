8db0471ddcfa8171b0fb0494526c5bc71dff7b99
Yaroslav <gso218695@gmail.com>
2021-08-19 21:22:19 +0300

    header done

--- views/public/scripts/main.js
+++ views/public/scripts/main.js
@@ -1,1 +1,14 @@
-'use strict';
+const toggler = document.getElementById('toggler');
+const nav = document.getElementById('nav');
+
+window.addEventListener('resize',function(){
+  if(window.innerWidth < 900) {
+    toggler.addEventListener('click', () => {
+      nav.classList.toggle('burger-menu__nav--active');
+      toggler.classList.toggle('burger-menu--active');
+    });
+  }
+});
+
+
+
