eb33ae603a0f41ed3a87e20f8a89800d77d0e198
Yaroslav <gso218695@gmail.com>
2021-08-20 13:09:15 +0300

    layout done

--- views/public/scripts/main.js
+++ views/public/scripts/main.js
@@ -1,5 +1,7 @@
 const toggler = document.getElementById('toggler');
 const nav = document.getElementById('nav');
+const carousel = document.getElementById('carouselExampleDark');
+const enableCarousel = document.getElementById('enable');
 
 window.addEventListener('resize',function(){
   if(window.innerWidth < 900) {
